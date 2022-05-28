97f56c70d653be85a54c6e84470d6d6ebf9420b2
ogata-higan <lviet456@gmail.com>
2022-05-28 16:41:15 +0700

    Update

--- app/src/main/java/com/example/ratingbar/MainActivity.java
+++ app/src/main/java/com/example/ratingbar/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.ratingbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-
+import android.widget.RatingBar;
+import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtValueRate;
+    RatingBar ratingBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControls();
+        addEvents();
+    }
+    private void addEvents() {
+        txtValueRate.setText(String.valueOf(ratingBar.getRating()));
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                txtValueRate.setText(String.valueOf(ratingBar.getRating()));
+
+            }
+        });
+
+    }
+    private void addControls() {
+        txtValueRate = findViewById(R.id.textViewValueRate);
+        ratingBar = findViewById(R.id.ratingBar);
+
     }
 }
